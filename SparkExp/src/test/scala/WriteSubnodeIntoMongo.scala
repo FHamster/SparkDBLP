1cc6d9aaf632339c06fe757c00b56023139568c4
FHamster <gaoxin459716010@163.com>
2020-07-07 07:52:11 +0800

    添加前缀提取

--- SparkExp/src/test/scala/WriteSubnodeIntoMongo.scala
+++ SparkExp/src/test/scala/WriteSubnodeIntoMongo.scala
@@ -1,5 +1,6 @@
 import com.mongodb.spark.MongoSpark
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.regexp_extract
 import org.scalatest.funsuite.AnyFunSuite
 
 
@@ -10,7 +11,8 @@ class WriteSubnodeIntoMongo extends AnyFunSuite {
   val charTest = "src/test/resources/article_CharTest.xml"
 
   test("article") {
-    import com.databricks.spark.xml._
+    import com.databricks.spark.xml.XmlDataFrameReader
+    import ss.implicits.StringToColumn
     val subnode = "article"
     val ss: SparkSession = SparkSession
       .builder
@@ -24,6 +26,8 @@ class WriteSubnodeIntoMongo extends AnyFunSuite {
       .option("rowTag", subnode)
       .schema(PropertiesObj.articleSchema)
       .xml(PropertiesObj.wholeDBLP_cvtSparkPath)
+      .withColumn("prefix", regexp_extract($"_key", "\\S*/\\S*/", 0))
+      .cache()
     opt.show()
     opt.printSchema()
 
@@ -33,7 +37,8 @@ class WriteSubnodeIntoMongo extends AnyFunSuite {
   }
 
   test("inproceedings") {
-    import com.databricks.spark.xml._
+    import com.databricks.spark.xml.XmlDataFrameReader
+    import ss.implicits.StringToColumn
     val subnode = "inproceedings"
     val ss: SparkSession = SparkSession
       .builder
@@ -41,12 +46,13 @@ class WriteSubnodeIntoMongo extends AnyFunSuite {
       .master("local[*]")
       .config("spark.mongodb.output.uri", s"mongodb://127.0.0.1/SparkDBLPTest.$subnode")
       .getOrCreate()
-
     val opt = ss.read
       .option("rootTag", "dblp")
       .option("rowTag", subnode)
       .schema(PropertiesObj.inproceedingsSchema)
       .xml(PropertiesObj.wholeDBLP_cvtSparkPath)
+      .withColumn("prefix", regexp_extract($"_key", "\\S*/\\S*/", 0))
+      .cache()
     opt.show()
     opt.printSchema()
 
@@ -55,7 +61,8 @@ class WriteSubnodeIntoMongo extends AnyFunSuite {
     ss.stop()
   }
   test("proceedings") {
-    import com.databricks.spark.xml._
+    import com.databricks.spark.xml.XmlDataFrameReader
+    import ss.implicits.StringToColumn
     val subnode = "proceedings"
     val ss: SparkSession = SparkSession
       .builder
@@ -69,6 +76,8 @@ class WriteSubnodeIntoMongo extends AnyFunSuite {
       .option("rowTag", subnode)
       .schema(PropertiesObj.proceedingsSchema)
       .xml(PropertiesObj.wholeDBLP_cvtSparkPath)
+      .withColumn("prefix", regexp_extract($"_key", "\\S*/\\S*/", 0))
+      .cache()
     opt.show()
     opt.printSchema()
 
@@ -77,7 +86,8 @@ class WriteSubnodeIntoMongo extends AnyFunSuite {
     ss.stop()
   }
   test("book") {
-    import com.databricks.spark.xml._
+    import com.databricks.spark.xml.XmlDataFrameReader
+    import ss.implicits.StringToColumn
     val subnode = "book"
     val ss: SparkSession = SparkSession
       .builder
@@ -91,6 +101,8 @@ class WriteSubnodeIntoMongo extends AnyFunSuite {
       .option("rowTag", subnode)
       .schema(PropertiesObj.bookSchema)
       .xml(PropertiesObj.wholeDBLP_cvtSparkPath)
+      .withColumn("prefix", regexp_extract($"_key", "\\S*/\\S*/", 0))
+      .cache()
     opt.show()
     opt.printSchema()
 
@@ -99,7 +111,8 @@ class WriteSubnodeIntoMongo extends AnyFunSuite {
     ss.stop()
   }
   test("incollection") {
-    import com.databricks.spark.xml._
+    import com.databricks.spark.xml.XmlDataFrameReader
+    import ss.implicits.StringToColumn
     val subnode = "incollection"
     val ss: SparkSession = SparkSession
       .builder
@@ -113,6 +126,8 @@ class WriteSubnodeIntoMongo extends AnyFunSuite {
       .option("rowTag", subnode)
       .schema(PropertiesObj.incollectionSchema)
       .xml(PropertiesObj.wholeDBLP_cvtSparkPath)
+      .withColumn("prefix", regexp_extract($"_key", "\\S*/\\S*/", 0))
+      .cache()
     opt.show()
     opt.printSchema()
 
@@ -121,7 +136,8 @@ class WriteSubnodeIntoMongo extends AnyFunSuite {
     ss.stop()
   }
   test("phdthesis") {
-    import com.databricks.spark.xml._
+    import com.databricks.spark.xml.XmlDataFrameReader
+    import ss.implicits.StringToColumn
     val subnode = "phdthesis"
     val ss: SparkSession = SparkSession
       .builder
@@ -135,6 +151,8 @@ class WriteSubnodeIntoMongo extends AnyFunSuite {
       .option("rowTag", subnode)
       .schema(PropertiesObj.phdthesisSchema)
       .xml(PropertiesObj.wholeDBLP_cvtSparkPath)
+      .withColumn("prefix", regexp_extract($"_key", "\\S*/\\S*/", 0))
+      .cache()
     opt.show()
     opt.printSchema()
 
@@ -143,7 +161,8 @@ class WriteSubnodeIntoMongo extends AnyFunSuite {
     ss.stop()
   }
   test("mastersthesis") {
-    import com.databricks.spark.xml._
+    import com.databricks.spark.xml.XmlDataFrameReader
+    import ss.implicits.StringToColumn
     val subnode = "incollection"
     val ss: SparkSession = SparkSession
       .builder
@@ -157,6 +176,8 @@ class WriteSubnodeIntoMongo extends AnyFunSuite {
       .option("rowTag", subnode)
       .schema(PropertiesObj.mastersthesisSchema)
       .xml(PropertiesObj.wholeDBLP_cvtSparkPath)
+      .withColumn("prefix", regexp_extract($"_key", "\\S*/\\S*/", 0))
+      .cache()
     opt.show()
     opt.printSchema()
 
@@ -165,7 +186,8 @@ class WriteSubnodeIntoMongo extends AnyFunSuite {
     ss.stop()
   }
   test("www") {
-    import com.databricks.spark.xml._
+    import com.databricks.spark.xml.XmlDataFrameReader
+    import ss.implicits.StringToColumn
     val subnode = "www"
     val ss: SparkSession = SparkSession
       .builder
@@ -179,6 +201,8 @@ class WriteSubnodeIntoMongo extends AnyFunSuite {
       .option("rowTag", subnode)
       .schema(PropertiesObj.wwwSchema)
       .xml(PropertiesObj.wholeDBLP_cvtSparkPath)
+      .withColumn("prefix", regexp_extract($"_key", "\\S*/\\S*/", 0))
+      .cache()
     opt.show()
     opt.printSchema()
 
@@ -186,4 +210,5 @@ class WriteSubnodeIntoMongo extends AnyFunSuite {
     MongoSpark.save(opt)
     ss.stop()
   }
+
 }
