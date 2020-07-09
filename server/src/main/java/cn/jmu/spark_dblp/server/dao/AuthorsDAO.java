243a6400476de88a1e49d2ef9c8efebfeabd4d39
FHamster <gaoxin459716010@163.com>
2020-07-09 08:29:41 +0800

    修复了不能分页的问题

--- server/src/main/java/cn/jmu/spark_dblp/server/dao/AuthorsDAO.java
+++ server/src/main/java/cn/jmu/spark_dblp/server/dao/AuthorsDAO.java
@@ -1,15 +1,14 @@
 package cn.jmu.spark_dblp.server.dao;
 
 import cn.jmu.spark_dblp.server.entity.Authors;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.stream.Stream;
-
 @Repository
 public interface AuthorsDAO extends PagingAndSortingRepository<Authors, String> {
-    Stream<Authors> findAllBy_VALUEContaining(String author);
+    Page<Authors> findAllBy_VALUEContaining(String author, Pageable pageable);
 
-    Stream<Authors> findTop10By_VALUEStartingWith(String prefix);
+    Page<Authors> findBy_VALUEStartingWith(String prefix, Pageable pageable);
 }
