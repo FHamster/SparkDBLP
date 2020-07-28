48304dcde1d93eda9b3a9931fbdfa058616f9f50
FHamster <gaoxin459716010@163.com>
2020-07-28 15:51:31 +0800

    journal index 加分页

--- server/src/main/java/cn/jmu/spark_dblp/server/dao/JournalIndexDAO.java
+++ server/src/main/java/cn/jmu/spark_dblp/server/dao/JournalIndexDAO.java
@@ -1,16 +1,16 @@
 package cn.jmu.spark_dblp.server.dao;
 
 import cn.jmu.spark_dblp.server.entity.JournalIndex;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface JournalIndexDAO extends PagingAndSortingRepository<JournalIndex, String> {
-    @Query(value="{journal:{$regex: '?0', $options: 'i'}}")
-    List<JournalIndex> findAllByJournalContaining(String journal);
+    @Query(value = "{journal:{$regex: '?0', $options: 'i'}}")
+    Page<JournalIndex> findAllByJournalContaining(String journal, Pageable pageable);
 
 //    Page<Venue> findBy_VALUEStartingWithIgnoreCase(String prefix, Pageable pageable);
 }
