3364d18c0d885db7062627e179342d8547d54875
FHamster <gaoxin459716010@163.com>
2021-03-02 21:03:29 +0800

    QueryConversionPipeline迁移至CacheService抽象类 Context相关操作方法迁移至CacheServiceScalaImpl子类 CacheServiceScalaImpl补充注释

--- server/src/main/java/cn/jmu/spark_dblp/server/service/CacheService.java
+++ server/src/main/java/cn/jmu/spark_dblp/server/service/CacheService.java
@@ -2,20 +2,13 @@ package cn.jmu.spark_dblp.server.service;
 
 
 import cn.jmu.spark_dblp.server.dao.OnlyDocDAO;
-import com.mongodb.client.MongoClients;
+import com.github.rutledgepaulv.rqe.pipes.QueryConversionPipeline;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 这个Service是为了实现对关键字的搜索结果进行缓存并将数据过滤工作由数据库上推至服务器层面
@@ -35,23 +28,12 @@ public abstract class CacheService {
     @Qualifier("soRedisTemplate")
     public RedisTemplate<String, Object> soRedisTemplate;
 
-    @CachePut(cacheNames = "context", key = "#md5")
-    public List<String> pushContext(String md5, List<String> context) {
-//        soRedisTemplate.opsForValue().set(md5, context);
-//        template.expire(md5, Duration.ofHours(2));
-        return context;
-    }
+    @Autowired
+//    @Qualifier("SparkDBLPMongoOperations")
+    MongoOperations mongoOps;
 
-    @Cacheable(cacheNames = "context", key = "#md5")
-    public List<String> getContext(String md5) {
-        return new ArrayList<>();
-    }
-
-    //    @Autowired
-    MongoOperations mongoOps = new MongoTemplate(
-            new SimpleMongoClientDatabaseFactory(MongoClients.create(),
-                    "SparkDBLPTest")
-    );
+    @Autowired
+    QueryConversionPipeline pipeline;
 
 
 }
--- server/src/main/java/cn/jmu/spark_dblp/server/service/CacheServiceScalaImpl.scala
+++ server/src/main/java/cn/jmu/spark_dblp/server/service/CacheServiceScalaImpl.scala
@@ -4,11 +4,11 @@ import cn.jmu.spark_dblp.server.entity.OnlyDoc
 import cn.jmu.spark_dblp.server.util.{ConditionUtil, InsensitiveMongoVisitor, InsensitivePredicateVisitor}
 import com.github.rutledgepaulv.qbuilders.builders.GeneralQueryBuilder
 import com.github.rutledgepaulv.qbuilders.conditions.Condition
-import com.github.rutledgepaulv.rqe.pipes.QueryConversionPipeline
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query.query
 import org.springframework.stereotype.Service
 
+import java.time.Duration
 import java.util
 import java.util.stream.Collectors
 import scala.annotation.tailrec
@@ -16,146 +16,133 @@ import scala.annotation.tailrec
 
 @Service
 class CacheServiceScalaImpl extends CacheService {
-  val pipeline: QueryConversionPipeline = QueryConversionPipeline.defaultPipeline
-
-  /*  def push(predicates: util.List[String]): Unit = {
-      val l: Iterator[String] = scala.collection.JavaConverters
-        .collectionAsScalaIterable(predicates)
-        .toIterator
-      //    recursivePush(md5, l)
-    }*/
-  /*
-
-    @nowarn
-    def push(md5: String, title: String): Unit = {
-      val list: util.List[OnlyDoc] = dao.findAllByTextReturnListJPA(title)
-      //    redisTemplate.opsForList leftPushAll(md5, list)
-      recursivePush(md5, Iterator(title))
-    }
-  */
-
-
-  /* @tailrec
-   final def recursivePush(key: String, predicates: Iterator[String])
-   : (String, Iterator[String]) = {
-     println(predicates.hasNext)
-     if (predicates.hasNext) {
-       template.opsForSet add(key, predicates.next)
-       recursivePush(key, predicates)
-     } else {
-       (key, predicates)
-     }
-
-   }
-
- */
   /**
-   * 获取缓存，或者查库
+   * 通过RSQL的上下文环境获取结果集
    *
-   * @param context
-   * @return
+   * @param RSQLContext RSQL上下文环境
+   * @return 根据RSQL上下文环境返回的结果集
    */
   def getOnlyDocListCache(RSQLContext: util.List[String]): util.List[OnlyDoc] = {
     // 缓存格式
     // 字典序context=>结果集
+    /**
+     *
+     * @param context       当前的查询上下文
+     * @param contextBuffer 查询上下文的暂存处
+     * @return 根据上下文环境返回的结果集
+     */
     @tailrec
     def get(context: List[String], contextBuffer: List[String]): util.List[OnlyDoc] = {
       //将timeline顺序的rsql序列序列化以后 parse
-      //      val condition = parseCondition(context)
 
-      //这里要求使用字典序的字符串形式
+      //为了保证上下文环境序列在映射缓存结果集的可交换性
+      //这里要求使用字典序的字符串形式（关键是顺序要统一）
+      //因为如果把集合作为key映射的时候由于集合无序性一个value会被多个key映射
+      //会很麻烦
       val result: util.List[OnlyDoc] =
-        if (context.nonEmpty) {
-          soRedisTemplate
-            .opsForValue()
-            .get(ConditionUtil.Condition2LexOrderString(toJavaList(context)))
-            .asInstanceOf[util.List[OnlyDoc]]
-        }
-        else null
+      if (context.nonEmpty) {
+        //上下文环境非空的时候
+        //尝试查缓存获取结果集
+        soRedisTemplate
+          .opsForValue()
+          .get(ConditionUtil.Condition2LexOrderString(toJavaList(context)))
+          .asInstanceOf[util.List[OnlyDoc]]
+      } else {
+        //此时上下文结果为空
+        null
+      }
 
 
       if (result != null) {
         //此时缓存命中
-        // 还要实现根据缓存和buffer进行过滤
+        //根据缓存和buffer进行过滤
         val restContext = parseCondition(context ++ contextBuffer.reverse)
         val temp: util.List[OnlyDoc] = result.parallelStream()
           .filter(restContext.query(new InsensitivePredicateVisitor[OnlyDoc]()))
           .collect(Collectors.toList[OnlyDoc])
 
-        // 加入缓存
-
-        val cacheKey: String = ConditionUtil
-          .Condition2LexOrderString(
-            toJavaList(context ++ contextBuffer.reverse)
-          )
-        soRedisTemplate.opsForValue().set(cacheKey, temp)
+        //加入缓存
+        soRedisTemplate.opsForValue().set(
+          generateCacheKey(context, contextBuffer),
+          temp
+        )
         temp
       } else if (context.isEmpty) {
-        //context没有断言的时候查库
-        // MongoDB搜索
+        //context没有断言的时候查MongoDB
+        //TODO 虽然能用，不够优雅，下次闲一点用建造器模式做会好点
 
+        //MongoDB的操作抽象
         val c: Criteria = parseCondition(contextBuffer)
           .query(new InsensitiveMongoVisitor)
 
         val temp = mongoOps.find(query(c), classOf[OnlyDoc])
-        val cacheKey: String = ConditionUtil
-          .Condition2LexOrderString(
-            toJavaList(context ++ contextBuffer.reverse)
-          )
-        soRedisTemplate.opsForValue().set(cacheKey, temp)
+
+        //存入缓存
+        soRedisTemplate.opsForValue().set(
+          generateCacheKey(context, contextBuffer),
+          temp
+        )
         temp
       } else {
         //context中至少一个断言时递归查找
+        //传入删去尾部的context参数
+        //buffer则需要暂存被删去的尾部
         get(
           context.take(context.size - 1),
           contextBuffer :+ context.last
         )
       }
     }
-
-    //转换工具
-    def toScalaList[A]: util.List[A] => List[A] = scala
-      .collection
-      .JavaConverters
-      .collectionAsScalaIterableConverter(_)
-      .asScala
-      .toList
-
-    //转换工具
-    def toJavaList[A]: List[A] => util.List[A] =
-      l => {
-        scala
-          .collection
-          .JavaConverters
-          .bufferAsJavaList(l.toBuffer)
-      }
-
-
-    //断言语法分析工具
-    def parseCondition: List[String] => Condition[GeneralQueryBuilder] = (l: List[String]) => pipeline
-      .apply(
-        toJavaList(l)
-          .stream
-          .collect(Collectors.joining(";")),
-        classOf[OnlyDoc]
-      )
-
+    //递归开始
     get(toScalaList(RSQLContext), List.empty[String])
   }
 
-  /*
-    @tailrec
-    final def recursiveSearch(docs: Stream[OnlyDoc], predicates: util.List[String])
-    : (Stream[OnlyDoc], util.List[String]) = {
-      if (predicates.isEmpty) {
-        (docs, predicates)
-      }
-      else {
-        val stringP = predicates.get(0)
-        predicates.remove(0)
-        val condition = pipeline.apply(stringP, classOf[OnlyDoc])
-        val predicate = condition.query(new PredicateVisitor[OnlyDoc])
-        recursiveSearch(docs.filter(predicate), predicates)
-      }
-    }*/
+  def pushContext(key: String, context: util.List[String]): Unit = {
+    soRedisTemplate.opsForValue.set(generateContextKey(key), context)
+    template.expire(key, Duration.ofHours(2))
+  }
+
+  /**
+   *
+   * @param key 作为获取
+   * @return 当缓存中不存在key值时候返回null
+   */
+  def getContext(key: String): util.List[String] =
+    soRedisTemplate.opsForValue.get(generateContextKey(key)).asInstanceOf[util.List[String]]
+
+  def deleteContext(key: String): Unit = soRedisTemplate.delete(generateContextKey(key))
+
+  def generateContextKey(key: String): String =
+    "context:" + key
+
+  //转换工具
+  def toScalaList[A]: util.List[A] => List[A] = scala
+    .collection
+    .JavaConverters
+    .collectionAsScalaIterableConverter(_)
+    .asScala
+    .toList
+
+  //转换工具
+  def toJavaList[A]: List[A] => util.List[A] = l => {
+    scala
+      .collection
+      .JavaConverters
+      .bufferAsJavaList(l.toBuffer)
+  }
+
+  //生成缓存的key
+  def generateCacheKey(context: List[String], contextBuffer: List[String]): String = ConditionUtil
+    .Condition2LexOrderString(
+      toJavaList(context ++ contextBuffer.reverse)
+    )
+
+  //断言语法分析工具
+  def parseCondition: List[String] => Condition[GeneralQueryBuilder] = (l: List[String]) => pipeline
+    .apply(
+      toJavaList(l)
+        .stream
+        .collect(Collectors.joining(";")),
+      classOf[OnlyDoc]
+    )
 }
