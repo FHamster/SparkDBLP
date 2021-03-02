2476580808406178a7470c44cb4f8d2414334c00
FHamster <gaoxin459716010@163.com>
2021-03-02 21:11:16 +0800

    删除废除的测试

--- server/src/test/java/cn/jmu/spark_dblp/server/service/CacheServiceScalaImplTest.java
+++ server/src/test/java/cn/jmu/spark_dblp/server/service/CacheServiceScalaImplTest.java
@@ -7,12 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.SetOperations;
 import org.springframework.test.annotation.Repeat;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.util.DigestUtils;
 
-import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -27,43 +24,6 @@ class CacheServiceScalaImplTest {
     CacheServiceScalaImpl cache;
     @Autowired
     OnlyDocDAO dao;
-
-    String title = "hadoop";
-
-/*    @Test
-    void recursiveSearchTest() {
-//        Stream<OnlyDoc> onlyDocStream = dao.findAllByTextReturnListJPA("hadoop").stream();
-//        List<String> p = new ArrayList<>();
-//        p.add("year>2010");
-//        p.add("year<2015");
-//        cache.recursiveSearch(onlyDocStream, p)._1.forEach(System.out::println);
-    }*/
-
-    SetOperations<String, String> so;
-    String hadoop = "hadoop";
-    String a = "a";
-    String b = "b";
-    String md5 = DigestUtils.md5DigestAsHex(title.getBytes(StandardCharsets.UTF_8));
-
-    //    @BeforeEach
-    void before() {
-        System.out.println("=================Test Start=================");
-
-        so = cache.template.opsForSet();
-
-        so.add(hadoop, "year>2019", "title=re=hadoop");
-        so.add(a, "year>2019");
-        so.add(b, "year>2019", "title=re=hadoop");
-    }
-
-    //    @AfterEach
-    void after() {
-        so.getOperations().delete(hadoop);
-        so.getOperations().delete(a);
-        so.getOperations().delete(b);
-        so.getOperations().delete(md5);
-        System.out.println("=================Test end=================");
-    }
 
     //缓存可继承性测试
     @Test
